ce7258f7b1eb05d2b2c8827c7b4759fdead0915b
AmeenAhmed <ameen.ahmed.b@gmail.com>
2013-04-18 16:15:50 +0530

    Lexing rules specified

--- lex.js
+++ lex.js
@@ -9,6 +9,68 @@ function addRule(rule, fn) {
 }
 
 
+var keywords = [
+
+	'var',
+	'function',
+	'class',
+	'private',
+	'public',
+	'elseif',
+	'if',
+	'else',
+	'switch',
+	'case',
+	'default',
+	'foreach',
+	'for',
+	'in',
+	'while',
+	'do',
+	'return',
+	'has',
+	'define',
+	'new',
+	'static',
+	'this',
+	'true',
+	'false',
+	'null',
+	'undefined'
+];
+
+var operators = [
+	
+	["<=",		"LE"],
+	[">=",		"GE"],
+	["<",		"LT"],
+	[">",		"GT"],
+	["==",		"EQ"],
+	["!=",		"NEQ"],
+	["<=",		"LE"],
+	["\\+",		"+"],
+	["-",		"-"],
+	["*",		"*"],
+	["\\/",		"/"],
+	[";",		";"],
+	["=",		"="],
+	["\\(",		"("],
+	["\\)",		")"],
+	["\\[",		"["],
+	["\\]",		"]"],
+	[",",		","],
+	[":",		":"],
+	["&&",		"AND"],
+	["\\|\\|",	"OR"],
+	["!",		"NOT"],
+	["\\{",		"{"],
+	["\\}",		"}"],
+	["\\.\\.",	".."],
+	["\\.",		"."],
+	["$",		"EOF"],
+
+];
+
 
 
 
@@ -27,9 +89,45 @@ exports.lexer = function() {
 		return '/* Skip white spaces */';
 	});
 
+	
+	var keywordsEnd = '(?![a-zA-Z0-9_$])';
+	for(var i=0; i<keywords.length; i++) {
+		(function(keyword) {
+			addRule(keyword + keywordsEnd, function() {
+				return keyword.toUpperCase();
+			});	
+		})(keywords[i]);
+		
+	}
+
+	addRule("[0-9]+(?:\\.[0-9]+)?\\b", function() {
+		return 'NUMBER';
+	});
+
+	addRule("\"[^\"]*\"", function() {
+		return 'STRING';
+	});
+
+	addRule("'[^'']*'", function() {
+		return 'STRING';
+	});
+
+	addRule("[a-zA-Z\\_\\$][a-zA-Z\\_0-9\\$]*", function() {
+		return 'IDENTIFIER';
+	});
+
+	for(var i=0; i<operators.length; i++) {
+		
+		(function(opr) {
+			addRule(opr[0], function() {
+				return opr[1];
+			});
+		})(operators[i]);
+	}
+
 	return lex;
 };
 
-console.log(exports.lexer());
+
 
 
